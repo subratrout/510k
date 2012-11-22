e723315af4f5cb3648bcefeca9edefcaa8d1c2c5
Subrat Rout <subratnrout@gmail.com>
2012-11-21 21:21:53 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fivetenk::Application.config.secret_token = 'c6acaaf45e3d313db2ab3ee23f66ed854456d97e0845d4c58eabdf03f149b8020eb7e7f2fdd08691ea5fa7605bb89b2b8b16769525a59f4195c8b30c90cd89d3'
